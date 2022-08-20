d88868b011abcf880ef653eb07af68f23b728e61
unknown <abanoob.meseha@gmail.com>
2022-08-20 12:44:22 +0200

    sum of two function

---
+++ sumOf2_function.js
@@ -0,0 +1,12 @@
+/**
+ * print the sum of two numbers 
+ * *********************************
+ * author : Abanoob Meseha
+ */
+function sumOf2(){
+    let inputNum1 = Number(prompt("Enter the first number :")),
+        inputNum2 = Number(prompt("Enter the second number :"));
+    console.log(`the sum of the two numbers is ${inputNum1 +inputNum2}`)
+}
+// function call
+sumOf2();

4b697c472036c294c025607ce4ede5bc9bdd9246
unknown <abanoob.meseha@gmail.com>
2022-08-20 11:24:37 +0200

    sum from 1 to 10 function

---
+++ sumFrom_1to10_function.js
@@ -0,0 +1,14 @@
+/**
+ * Compute The sum of the numbers from 1 to 10 
+ * *****************************************
+ * author : Abanoob Meseha 
+ */
+function sumFrom1to10(){
+    let sum = 0;
+    for(let i=1; i <=10 ;i++){
+        sum +=i;
+    }
+    console.log(`the sum from 1 to 10 is ${sum}`)
+}
+// function call
+sumFrom1to10();
